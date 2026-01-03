8cba0adb32c90476cbbe747c0f3e35790f1f33ec
Shaun67789 <133377835+Shaun67789@users.noreply.github.com>
2026-01-04 00:04:52 +0600

    Refactor cleanup loop to run in a thread
    Refactor cleanup function to use threading and handle exceptions.

--- cleanup.py
+++ cleanup.py
@@ -1,13 +1,20 @@
-import os, time, shutil
+import os, time, threading
 
 DOWNLOAD_DIR = "downloads"
-MAX_AGE = 20 * 60
+MAX_AGE = 20 * 60  # 20 minutes
 
-def cleanup_loop():
+def _cleanup_loop():
     while True:
         now = time.time()
         for f in os.listdir(DOWNLOAD_DIR):
             path = os.path.join(DOWNLOAD_DIR, f)
             if os.path.isfile(path) and now - os.path.getmtime(path) > MAX_AGE:
-                os.remove(path)
+                try:
+                    os.remove(path)
+                except:
+                    pass
         time.sleep(300)
+
+def start_cleaner():
+    t = threading.Thread(target=_cleanup_loop, daemon=True)
+    t.start()
